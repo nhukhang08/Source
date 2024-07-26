927d7477221ff330d0af7e844b942164a5a60189
nhukhang08 <nhukhangdoilau@gmail.com>
2024-07-26 15:51:56 +0700

    main

--- Exercise/code.py
+++ Exercise/code.py
@@ -2,7 +2,7 @@ f, g = open("input.txt", "r"), open("output.txt", "w")
 n = int(f.readline())
 def divisor(n):
     result = []
-    for i in range(2, int(n**0.5)+1): # Duyệt các số từ 2 đến n**0.5
+    for i in range(1, int(n**0.5)+1): # Duyệt các số từ 2 đến n**0.5
         if n % i == 0: # Kiểm tra ước
             result.append(i)
             # Kiểm tra ước đối
@@ -11,15 +11,18 @@ def divisor(n):
     return result
 
 def createMul(n):
+    if n == 4:
+        return [[1, 4], [2, 2]]
     div = divisor(n)
     if len(div) == 1:
-        return [div[-1], div[-1]]
+        return  [ [div[-1], div[-1]] ]
+
     result = []
     for i in range(len(div) - 1):
         for j in range(i + 1, len(div)):
-
             if div[i] * div[j] == n:
-                x, y = div[i], div[j]
+                x = min(div[i], div[j])
+                y = max(div[i], div[j])
                 result.append([x, y])
     return result
 
@@ -27,30 +30,48 @@ def analysic(n):
     result = []
     for mul in createMul(n):
         x, y = mul
-        result.append((x - 1) * (y + 1))
+        num = (x - 1) * (y + 1)
+        result.append(num)
     return result
 
+
 result = []
+def processNum(arr: list):
+    arr.sort(reverse=True)
+    for num in arr:
+        if num == 0:
+            return result
+        result.append(num)
+    processNum(result)
+    return result
+
+arr = []
 for num in analysic(n):
-    result.append(num)
+    if num != 0:
+        arr.append(num)
+
+def transpose(x: int):
+    def backtrack(path, options):
+        if len(options) == 1:
+            return path
+        for i in range(len(options)):
+            pass
 
 print(result)
 
 
+# for num in analysic(n):
+#     result.append(num)
+#     while len(analysic(num)) != 0:
+#         for num in analysic(num):
+#             if num not in result:
+#                 result.append(num)
+#
 # result.sort()
-# l = len(result)
-# g.write(str(l) + "\n")
 #
-# for line in range(l // 10):
-#     for i in range(10):
-#         g.write(str(result[line+i]))
-#     i += 1
-#     if i % 10 == 0:
-#         g.write("\n")
-#
-# for i in range(l-1):
-#     g.write(f"{result[i]} ")
-# g.write(str(result[-1]))
-#
-#
-# f.close(); g.close()
+
+# g.write(f"{len(result)}\n")
+# for i in range(0, len(result), 10):
+#     g.write(" ".join(map(str, result[i:i + 10])) + "\n")
+
+f.close(); g.close()
