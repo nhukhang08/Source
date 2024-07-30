596d6aaaed4b5691f6fde36a1327b6432d9ecb24
nhukhang08 <nhukhangdoilau@gmail.com>
2024-07-30 15:19:41 +0700

    main

--- Exercise/code.py
+++ Exercise/code.py
@@ -1,13 +1,1 @@
-with open("input.txt", "r") as f:
-    n, m = map(int, f.readline().split())
-s = [1]
-for i in range(n):
-    result = []
-    while len(s)!=0:
-        num = s.pop()
-        result.append(2*num+1)
-        result.append(3*num+1)
-    s = result
-result.sort()
-with open("output.txt", "w") as g:
-    g.write(str(result[m-1]))
+g
