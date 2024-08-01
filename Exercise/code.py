ad1d8353043c21c07ea6953c446db0a421ef923f
nhukhang08 <nhukhangdoilau@gmail.com>
2024-08-01 13:43:55 +0700

    main

--- Exercise/code.py
+++ Exercise/code.py
@@ -3,18 +3,19 @@ n = 3
 
 def BHK(n):
     list_num = [2, 7]
+    # 
     div = {}
     while list_num: 
         num = list_num.pop(0)
         if num % n == 0:
             return num
-        if num%n not in div:
+        if num%n in div:
+            for num in "027":
+                list_num.append( int ( str(list_num[0]) + num ) % n )
+                if int ( str(list_num[0]) + num ) % n == 0:
+                    return int ( str(list_num[0]) + num )
+        else:
             div[num%n] = num
-        print(div)
 
-if __name__ == "__main__":
-    BHK(n)
-        
-        
-        
-        
+
+print(BHK(n))
